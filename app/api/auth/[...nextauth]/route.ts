11dda5e65cb16ad6ebc57d75e34988fc51588e56
Nan <caonanla@gmail.com>
2024-02-19 13:34:57 +0100

    remove authOptions to utils folder

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,57 +5,58 @@ import prismadb from "@/lib/prismadb";
 import bcrypt from "bcryptjs";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import { authOptions } from "@/lib/utils";
 
 const prisma = new PrismaClient();
 
-export const authOptions: NextAuthOptions = {
-  pages: {
-    signIn: "sign-in",
-  },
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXT_AUTH_SECRET,
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    CredentialsProvider({
-      name: "Credentials",
-      credentials: {
-        email: {
-          label: "Email",
-          type: "email",
-          placeholder: "john@mail.com",
-        },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials) {
-        if (!credentials?.email || !credentials?.password) {
-          throw new Error("Email or password is empty!");
-        }
+// export const authOptions: NextAuthOptions = {
+//   pages: {
+//     signIn: "sign-in",
+//   },
+//   session: {
+//     strategy: "jwt",
+//   },
+//   secret: process.env.NEXT_AUTH_SECRET,
+//   adapter: PrismaAdapter(prisma),
+//   providers: [
+//     CredentialsProvider({
+//       name: "Credentials",
+//       credentials: {
+//         email: {
+//           label: "Email",
+//           type: "email",
+//           placeholder: "john@mail.com",
+//         },
+//         password: { label: "Password", type: "password" },
+//       },
+//       async authorize(credentials) {
+//         if (!credentials?.email || !credentials?.password) {
+//           throw new Error("Email or password is empty!");
+//         }
 
-        const user = await prismadb.user.findFirst({
-          where: {
-            email: credentials.email,
-          },
-        });
+//         const user = await prismadb.user.findFirst({
+//           where: {
+//             email: credentials.email,
+//           },
+//         });
 
-        if (!user || !user.id || !user.password) {
-          throw new Error("Invalid user!");
-        }
-        const machtedPassword = await bcrypt.compare(
-          credentials.password,
-          user.password
-        );
+//         if (!user || !user.id || !user.password) {
+//           throw new Error("Invalid user!");
+//         }
+//         const machtedPassword = await bcrypt.compare(
+//           credentials.password,
+//           user.password
+//         );
 
-        if (!machtedPassword) {
-          throw new Error("Invalid password");
-        }
+//         if (!machtedPassword) {
+//           throw new Error("Invalid password");
+//         }
 
-        return user;
-      },
-    }),
-  ],
-};
+//         return user;
+//       },
+//     }),
+//   ],
+// };
 
 const handler = NextAuth(authOptions);
 
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,3 +1,9 @@
+import CredentialsProvider from "next-auth/providers/credentials";
+import prismadb from "@/lib/prismadb";
+import bcrypt from "bcryptjs";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { NextAuthOptions } from "next-auth";
+
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -9,3 +15,52 @@ export const formatter = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
 });
+
+export const authOptions: NextAuthOptions = {
+  pages: {
+    signIn: "sign-in",
+  },
+  session: {
+    strategy: "jwt",
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "john@mail.com",
+        },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (!credentials?.email || !credentials?.password) {
+          throw new Error("Email or password is empty!");
+        }
+
+        const user = await prismadb.user.findFirst({
+          where: {
+            email: credentials.email,
+          },
+        });
+
+        if (!user || !user.id || !user.password) {
+          throw new Error("Invalid user!");
+        }
+        const machtedPassword = await bcrypt.compare(
+          credentials.password,
+          user.password
+        );
+
+        if (!machtedPassword) {
+          throw new Error("Invalid password");
+        }
+
+        return user;
+      },
+    }),
+  ],
+};
